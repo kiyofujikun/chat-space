c5bdfdeb59493f0c9b6abc239fbc76689f30f778
kiyofujikun <kypommg@gmail.com>
2020-06-16 17:19:45 +0900

    JavaScript

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="Message__box">
+          <div class="Message__info">
+            <div class="Message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="Message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Messages">
+            <p class="Message__comment">
+              ${message.content}
+            </p>
+            <img class="Message__img" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="Message__box">
+        <div class="Message__info">
+          <div class="Message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="Message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Messages">
+          <p class="Message__comment">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.Form__contents').on('submit', function(e){
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.Message').append(html);
+      $('.Message').animate({ scrollTop: $('.Message')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.Form__submitBtn').prop("disabled", false);
+    })
+    .fail(function(){
+      alert("送信に失敗しました");
+      $('.Form__submitBtn').prop("disabled", false);
+    })
+  });
+});
